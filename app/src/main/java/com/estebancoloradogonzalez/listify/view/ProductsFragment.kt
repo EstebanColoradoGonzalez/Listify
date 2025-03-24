c9ec454a0386b5df858390e158af574ebbccd6bb
EstebanColoradoGonzalez <estebancoloradogonzalez@gmail.com>
2025-03-24 12:39:24 -0500

    Feat: Add buttons to navigate to category fragments and create products.

--- app/src/main/java/com/estebancoloradogonzalez/listify/view/ProductsFragment.kt
+++ app/src/main/java/com/estebancoloradogonzalez/listify/view/ProductsFragment.kt
@@ -1,13 +1,13 @@
 package com.estebancoloradogonzalez.listify.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.estebancoloradogonzalez.listify.R
+import androidx.navigation.fragment.findNavController
 import com.estebancoloradogonzalez.listify.databinding.FragmentProductsBinding
 import com.estebancoloradogonzalez.listify.utils.NumericConstants
 import com.estebancoloradogonzalez.listify.viewmodel.UserViewModel
@@ -15,14 +15,16 @@ import kotlinx.coroutines.launch
 
 class ProductsFragment : Fragment() {
     private var _binding: FragmentProductsBinding? = null
+    private val binding get() = _binding!!
     private val userViewModel: UserViewModel by viewModels()
     private var userId = NumericConstants.LONG_NEGATIVE_ONE
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_products, container, false)
+    ): View {
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +32,16 @@ class ProductsFragment : Fragment() {
 
         lifecycleScope.launch {
             userId = userViewModel.getUserId()
+        }
+
+        binding.fabAddProduct.setOnClickListener {
+            val action = ProductsFragmentDirections.actionProductsFragmentToCreateProductFragment(userId)
+            findNavController().navigate(action)
+        }
+
+        binding.btnCategories.setOnClickListener {
+            val action = ProductsFragmentDirections.actionProductsFragmentToCategoriesFragment(userId)
+            findNavController().navigate(action)
         }
     }
 
