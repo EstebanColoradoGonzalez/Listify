8497de372835502ad2a0584a6b1f1ce3cb7db22a
EstebanColoradoGonzalez <estebancoloradogonzalez@gmail.com>
2025-05-18 15:59:06 -0500

    Refactor: Adjust test data

---
+++ app/src/main/java/com/estebancoloradogonzalez/listify/model/database/data/ProductSeed.kt
@@ -0,0 +1,11 @@
+package com.estebancoloradogonzalez.listify.model.database.data
+
+data class ProductSeed(
+    val name: String,
+    val price: String,
+    val quantity: String,
+    val unit: String,
+    val frequency: String,
+    val store: String,
+    val category: String
+)
---
+++ app/src/main/java/com/estebancoloradogonzalez/listify/model/database/utils/ProductData.kt
@@ -0,0 +1,91 @@
+package com.estebancoloradogonzalez.listify.model.database.utils
+
+object ProductData {
+    object Tocineta { const val NAME = "Tocineta"; const val PRICE = "6300"; const val QUANTITY = "1" }
+    object Jamon { const val NAME = "Jamon"; const val PRICE = "8500"; const val QUANTITY = "1" }
+    object PanTajado { const val NAME = "Pan Tajado"; const val PRICE = "6490"; const val QUANTITY = "1" }
+    object QuesoMozarellaEnLonchas { const val NAME = "Queso Mozarella en Lonchas"; const val PRICE = "9490"; const val QUANTITY = "1" }
+    object QuesoCrema { const val NAME = "Queso Crema"; const val PRICE = "3300"; const val QUANTITY = "1" }
+    object SalsaBBQ { const val NAME = "Salsa BBQ"; const val PRICE = "6800"; const val QUANTITY = "1" }
+    object SalsaAguacate { const val NAME = "Salsa Aguacate"; const val PRICE = "3490"; const val QUANTITY = "1" }
+    object Salchichas { const val NAME = "Salchichas"; const val PRICE = "6100"; const val QUANTITY = "1" }
+    object CremaDental { const val NAME = "Crema Dental"; const val PRICE = "5850"; const val QUANTITY = "2" }
+    object Suavizante { const val NAME = "Suavizante"; const val PRICE = "8990"; const val QUANTITY = "1" }
+    object GalletasParaNiebla { const val NAME = "Galletas para Niebla"; const val PRICE = "2200"; const val QUANTITY = "1" }
+    object GalletasParaNube { const val NAME = "Galletas para Nube"; const val PRICE = "2900"; const val QUANTITY = "1" }
+    object Atun { const val NAME = "Atún"; const val PRICE = "3450"; const val QUANTITY = "2" }
+    object Aceite { const val NAME = "Aceite"; const val PRICE = "27950"; const val QUANTITY = "1" }
+    object Nescafe { const val NAME = "Nescafe"; const val PRICE = "12650"; const val QUANTITY = "1" }
+    object Ducales { const val NAME = "Ducales"; const val PRICE = "5700"; const val QUANTITY = "1" }
+    object CremaDeLeche { const val NAME = "Crema de Leche"; const val PRICE = "2190"; const val QUANTITY = "1" }
+    object Mantequilla { const val NAME = "Mantequilla"; const val PRICE = "4700"; const val QUANTITY = "1" }
+    object Azucar { const val NAME = "Azucar"; const val PRICE = "3990"; const val QUANTITY = "2" }
+    object Sal { const val NAME = "Sal"; const val PRICE = "2490"; const val QUANTITY = "1" }
+    object SnackCremoso { const val NAME = "Snack Cremoso"; const val PRICE = "7990"; const val QUANTITY = "1" }
+    object BolsasDeBasuraPequenas { const val NAME = "Bolsas de Basura Pequeñas"; const val PRICE = "2190"; const val QUANTITY = "1" }
+    object BolsasDeBasuraGrandes { const val NAME = "Bolsas de Basura Grandes"; const val PRICE = "2200"; const val QUANTITY = "2" }
+    object DetergenteRopaOscura { const val NAME = "Detergente Ropa Oscura"; const val PRICE = "6300"; const val QUANTITY = "1" }
+    object DetergenteLiquido { const val NAME = "Detergente Liquido"; const val PRICE = "12600"; const val QUANTITY = "1" }
+    object JabonTocador { const val NAME = "Jabón tocador"; const val PRICE = "5990"; const val QUANTITY = "1" }
+    object Ramen { const val NAME = "Ramén"; const val PRICE = "2490"; const val QUANTITY = "1" }
+    object PapelHigenico { const val NAME = "Papel Higenico"; const val PRICE = "13940"; const val QUANTITY = "1" }
+    object Limpiavidrios { const val NAME = "Limpiavidrios"; const val PRICE = "2250"; const val QUANTITY = "1" }
+    object LimpiadorBicarbonato { const val NAME = "Limpiador Bicarbonato"; const val PRICE = "2600"; const val QUANTITY = "1" }
+    object PanoLimpon { const val NAME = "Paño Limpon"; const val PRICE = "3400"; const val QUANTITY = "1" }
+    object Alcohol { const val NAME = "Alcohol"; const val PRICE = "2900"; const val QUANTITY = "3" }
+    object PastillasParaBano { const val NAME = "Pastillas para baño"; const val PRICE = "2900"; const val QUANTITY = "1" }
+
+    object Desodorante { const val NAME = "Desodorante"; const val PRICE = "12000"; const val QUANTITY = "1" }
+    object Listerine { const val NAME = "Listerine"; const val PRICE = "14000"; const val QUANTITY = "1" }
+    object JabonLiquidoDeManos { const val NAME = "Jabón liquido de manos"; const val PRICE = "25000"; const val QUANTITY = "1" }
+    object EsponjaParaBrillarOllas { const val NAME = "Esponja para brillar ollas"; const val PRICE = "3000"; const val QUANTITY = "1" }
+    object EsponjaParaTrastes { const val NAME = "Esponja para trastes"; const val PRICE = "5000"; const val QUANTITY = "1" }
+    object Limpido { const val NAME = "Limpido"; const val PRICE = "8000"; const val QUANTITY = "2" }
+    object Brillantismo { const val NAME = "Brillantismo"; const val PRICE = "6000"; const val QUANTITY = "1" }
+    object JabonParaTrastes { const val NAME = "Jabón para Trastes"; const val PRICE = "12000"; const val QUANTITY = "1" }
+    object AmbientadorDeArenaDeGatos { const val NAME = "Ambientador de arena de gatos"; const val PRICE = "6000"; const val QUANTITY = "1" }
+    object Aluminio { const val NAME = "Aluminio"; const val PRICE = "6000"; const val QUANTITY = "1" }
+
+    object CarneDeRes { const val NAME = "Carne de Res"; const val PRICE = "34000"; const val QUANTITY = "1.81" }
+    object Chorizos { const val NAME = "Chorizos"; const val PRICE = "2500"; const val QUANTITY = "4" }
+    object SolomitoDeCerdo { const val NAME = "Solomito de Cerdo"; const val PRICE = "21000"; const val QUANTITY = "0.9" }
+    object Alitas { const val NAME = "Alitas"; const val PRICE = "17000"; const val QUANTITY = "1.36" }
+    object Lagarto { const val NAME = "Lagarto"; const val PRICE = "25000"; const val QUANTITY = "1.36" }
+    object Chicharron { const val NAME = "Chicharrón"; const val PRICE = "28000"; const val QUANTITY = "1.81" }
+
+    object Maracuya { const val NAME = "Maracuya"; const val PRICE = "4000"; const val QUANTITY = "2.4" }
+    object Mango { const val NAME = "Mango"; const val PRICE = "3800"; const val QUANTITY = "3.5" }
+    object Ajo { const val NAME = "Ajo"; const val PRICE = "7000"; const val QUANTITY = "0.2" }
+    object Pimenton { const val NAME = "Pimenton"; const val PRICE = "2500"; const val QUANTITY = "1" }
+    object Apio { const val NAME = "Apio"; const val PRICE = "14000"; const val QUANTITY = "0.2" }
+    object Platano { const val NAME = "Platano"; const val PRICE = "2800"; const val QUANTITY = "1.4" }
+    object Pepino { const val NAME = "Pepino"; const val PRICE = "3000"; const val QUANTITY = "1" }
+    object Tomate { const val NAME = "Tomate"; const val PRICE = "2500"; const val QUANTITY = "3" }
+    object Lechuga { const val NAME = "Lechuga"; const val PRICE = "2000"; const val QUANTITY = "1" }
+    object Zanahoria { const val NAME = "Zanahoria"; const val PRICE = "1800"; const val QUANTITY = "2" }
+    object CebollaDeHuevo { const val NAME = "Cebolla de Huevo"; const val PRICE = "5300"; const val QUANTITY = "2" }
+    object PapasCriollas { const val NAME = "Papas criollas"; const val PRICE = "5000"; const val QUANTITY = "1" }
+    object Limones { const val NAME = "Limones"; const val PRICE = "3500"; const val QUANTITY = "2.3" }
+    object Naranjas { const val NAME = "Naranjas"; const val PRICE = "3000"; const val QUANTITY = "2.8" }
+    object Repollo { const val NAME = "Repollo"; const val PRICE = "3380"; const val QUANTITY = "1" }
+    object Papas { const val NAME = "Papas"; const val PRICE = "3000"; const val QUANTITY = "7" }
+    object Cilantro { const val NAME = "Cilantro"; const val PRICE = "7000"; const val QUANTITY = "0.29" }
+    object Arverja { const val NAME = "Arverja"; const val PRICE = "6500"; const val QUANTITY = "0.62" }
+    object SalDeAjo { const val NAME = "Sal de Ajo"; const val PRICE = "1000"; const val QUANTITY = "1" }
+    object Pimienta { const val NAME = "Pimienta"; const val PRICE = "1000"; const val QUANTITY = "1" }
+    object OreganoEnPolvo { const val NAME = "Oregano en Polvo"; const val PRICE = "2000"; const val QUANTITY = "1" }
+    object SalDeCebolla { const val NAME = "Sal de Cebolla"; const val PRICE = "1000"; const val QUANTITY = "1" }
+
+    object Shampoo { const val NAME = "Shampoo"; const val PRICE = "40000"; const val QUANTITY = "1" }
+    object CremaDepilacion { const val NAME = "Crema Depilación"; const val PRICE = "30000"; const val QUANTITY = "1" }
+    object ComidaParaNiebla { const val NAME = "Comida para Niebla"; const val PRICE = "12500"; const val QUANTITY = "1" }
+    object ComidaParaNube { const val NAME = "Comida para Nube"; const val PRICE = "18000"; const val QUANTITY = "1" }
+    object Leche { const val NAME = "Leche"; const val PRICE = "3500"; const val QUANTITY = "1" }
+    object SalchichonCervecero { const val NAME = "Salchichon Cervecero"; const val PRICE = "22000"; const val QUANTITY = "1" }
+    object BolsasTransparentes { const val NAME = "Bolsas Transparentes"; const val PRICE = "8000"; const val QUANTITY = "1" }
+    object Arroz { const val NAME = "Arroz"; const val PRICE = "2100"; const val QUANTITY = "6" }
+    object SalsaDeTomate { const val NAME = "Salsa de Tomate"; const val PRICE = "16000"; const val QUANTITY = "1" }
+    object Mayonesa { const val NAME = "Mayonesa"; const val PRICE = "16000"; const val QUANTITY = "1" }
+    object Huevos { const val NAME = "Huevos"; const val PRICE = "17000"; const val QUANTITY = "1" }
+    object ArenaParaLaGata { const val NAME = "Arena para la Gata"; const val PRICE = "24000"; const val QUANTITY = "1" }
+}
